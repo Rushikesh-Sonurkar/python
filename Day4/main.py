c4909c5f6681804fcb801980bb99f6459f8da78c
Rushikesh Sonurkar <navyofficer65@gmail.com>
2025-08-21 10:29:43 +0530

    Create main.py

---
+++ Day4/main.py
@@ -0,0 +1,15 @@
+# Day 4 - Our First Python Program
+# Task: Print a poem using Python
+
+print("🌿 The Road Not Taken 🌿")
+print("Two roads diverged in a yellow wood,")
+print("And sorry I could not travel both")
+print("And be one traveler, long I stood")
+print("And looked down one as far as I could")
+print("To where it bent in the undergrowth;")
+print("")
+print("Then took the other, as just as fair,")
+print("And having perhaps the better claim,")
+print("Because it was grassy and wanted wear;")
+print("Though as for that the passing there")
+print("Had worn them really about the same.")
